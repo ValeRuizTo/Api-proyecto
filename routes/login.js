fe4a27af823097f5295932cc3e066a3a56484730
Valentina Ruiz <valentinaruito@unisabana.edu.co>
2024-05-05 19:03:18 -0500

    commit Login

--- routes/login.js
+++ routes/login.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-
 router.post("/", (req, res) => {
   const lowercaseBody = {};
   for (let key in req.body) {
@@ -14,16 +13,19 @@ router.post("/", (req, res) => {
     return res.status(400).json({ error: 'Los parámetros son obligatorios.' });
   }
 
-
   const usuario = {
     username,
     password,
   };
 
-  res.status(201).json(usuario);
-  res.json({ message: 'Login successful' });
+  // Combinar ambas respuestas en un solo objeto JSON
+  const response = {
+    usuario,
+    message: 'Login successful'
+  };
 
+  // Enviar la respuesta combinada
+  res.status(201).json(response);
 });
-
 
 module.exports = router;
