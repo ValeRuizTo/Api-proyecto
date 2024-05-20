d2cfbb17bdf9e1dc207c69a772491f5a486f9834
Valentina Ruiz <valentinaruito@unisabana.edu.co>
2024-05-19 20:27:39 -0500

    commit tweets

--- routes/tweets.js
+++ routes/tweets.js
@@ -6,33 +6,31 @@ const jwtMiddleware = require('../middleware/jwtMiddleware');
 // Ruta GET para obtener todos los tweets con el nombre de usuario y hashtag
 router.get("/", jwtMiddleware, async (req, res) => {
   try {
-    // Obtener todos los usuarios de la colección 'users' en Firestore
     const usersSnapshot = await admin.firestore().collection('users').get();
 
-    let tweetsWithUsernamesAndHashtags = [];
-    // Iterar sobre cada usuario
+    let info = [];
     for (const userDoc of usersSnapshot.docs) {
       const userData = userDoc.data();
       // Obtener el nombre de usuario del usuario
       const username = userData.username;
       // Obtener la lista de tweets del usuario
-      const userTweets = userData.tweets || []; // Suponiendo que 'tweets' es un campo en la colección de usuarios que almacena la lista de tweets
+      const userTweets = userData.tweets || []; 
       // Construir un objeto que contenga el tweet, el nombre de usuario y el hashtag para cada tweet del usuario
       for (const tweet of userTweets) {
         // Verificar si el tweet es un objeto o una cadena de texto
         const tweetObject = typeof tweet === 'object' ? tweet : { tweet }; // Si es un objeto, usa el mismo, de lo contrario, crea un objeto con la cadena de texto del tweet
-        const tweetWithUsernameAndHashtag = {
+        const data = {
           tweet: tweetObject.tweet,
           username: username,
-          hashtag: tweetObject.hashtag || '' // Si no hay hashtag, usar cadena vacía
+          hashtag: tweetObject.hashtag 
         };
         // Agregar este objeto a la lista de tweets con nombres de usuario y hashtags
-        tweetsWithUsernamesAndHashtags.push(tweetWithUsernameAndHashtag);
+        info.push(data);
       }
     }
 
     // Devolver la lista de tweets con nombres de usuario y hashtags como respuesta
-    res.status(200).json(tweetsWithUsernamesAndHashtags);
+    res.status(200).json(info);
   } catch (error) {
     console.error('Error al obtener tweets con nombres de usuario y hashtags:', error);
     res.status(500).json({ error: 'Ocurrió un error al obtener tweets con nombres de usuario y hashtags.' });
