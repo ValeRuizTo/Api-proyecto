41204efeac5d2ae9a13eaf35bdfb3d15cdc56233
Valentina Ruiz <valentinaruito@unisabana.edu.co>
2024-05-20 21:14:03 -0500

    x

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require('cors'); // Importa el paquete cors
 
 const app = express();
-
+/* 
 //Configuración del middleware CORS
 app.use(cors({
   origin: 'http://localhost:5173',
@@ -11,7 +11,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 const allowedOrigins = ['http://localhost:5173', 'https://social-garden.vercel.app'];
-/* 
+*/
 app.use(cors({
   origin: function(origin, callback) {
     // Verifica si la solicitud proviene de una de las URLs permitidas
@@ -26,7 +26,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-*/
+
 
 app.use(express.urlencoded({ extended: true })); // para acceder al body
 app.use(express.json());
